a008b031751d3bee73870f8f10043952f9dbd5ec
Seth David Gyimah <esec26@gmail.com>
2023-09-23 20:19:15 +0000

    completed post patch delete lab

--- server/app.py
+++ server/app.py
@@ -30,17 +30,55 @@ def bakeries():
     )
     return response
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
 def bakery_by_id(id):
 
+    # step 1: find bakery using id
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
 
-    response = make_response(
-        bakery_serialized,
-        200
-    )
-    return response
+    # step 2: Define General if not found REsponse
+    if not bakery:
+        response = make_response(
+            'bakery not found',
+            400
+        )
+
+        return response
+
+    # handling GET Requests
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
+
+        response = make_response(
+            bakery_serialized,
+            200
+        )
+        return response
+    
+    # handling PATCH Requests
+    elif request.method == 'PATCH':
+
+        # step 1: iterate through form,  for every key in form, 
+            # find corresponding key in review and set it to value of key in form
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+
+        # step 2: add UPDATED instance to to session and commit to session
+        db.session.add(bakery)
+        db.session.commit()
+
+        # step 3: convert UPDATED instance to dictionary and return it alongside status code
+        bakery_dict = bakery.to_dict()
+
+        response = make_response(
+            bakery_dict,
+            200
+        )
+
+        return response
+
+
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -65,6 +103,91 @@ def most_expensive_baked_good():
         200
     )
     return response
+
+
+@app.route('/baked_goods', methods=['GET', 'POST'])
+def baked_goods():
+
+    # handling GET Request
+    if request.method == 'GET':
+        baked_goods = BakedGood.query.all()
+
+        baked_goods_array = []
+
+        for baked_good in baked_goods:
+
+            baked_goods_dict = {
+                'id': baked_good.id,
+                'name': baked_good.name,
+                'price': baked_good.price,
+                'created_at': baked_good.created_at
+            }
+            baked_goods_array.append(baked_goods_dict)
+        
+        response = make_response(
+            jsonify(baked_goods_array),
+            200
+        )
+        return response
+
+    # handling POST Request
+    elif request.method == 'POST':
+
+        # step 1: extract values from form to create new Review Instance
+        new_baked_good = BakedGood(
+            name = request.form.get('name'),
+            price = request.form.get('price'),
+            bakery_id = request.form.get('bakery_id')
+
+        )
+
+        # step 2: add new instance to to session and commit to session
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        # step 3: convert new instance to dictionary and return it alongside status code
+        new_baked_good_dict = new_baked_good.to_dict()
+
+        response = make_response(
+            new_baked_good_dict,
+            201
+        )
+
+        return response
+
+
+@app.route('/baked_goods/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def baked_good_by_id(id):
+
+    # step 1 find baked_good using id
+    baked_good =  BakedGood.query.filter(BakedGood.id == id).first()
+
+     # step 2: Define General if not found REsponse
+    if not baked_good:
+        response = make_response(
+            'baked_good not found',
+            400
+        )
+
+        return response
+    
+    # step 3: handle GET Request
+    if request.method == 'DELETE':
+        db.session.delete(baked_good)
+        db.session.commit()
+
+        response_body = {
+            'delete successful': True,
+            'message': 'Review successfully deleted'
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
